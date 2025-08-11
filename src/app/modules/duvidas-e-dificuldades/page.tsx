b4b55fea816e4efd6377122aaec921fdac4d9b5c
Nemo top <ronansantiagodosres444@gmail.com>
2025-08-11 12:40:57 +0000

    Nesse modulo /modules/duvidas-e-dificuldades
    Tem esse botão: 🧠 Tenho p

--- src/app/modules/duvidas-e-dificuldades/page.tsx
+++ src/app/modules/duvidas-e-dificuldades/page.tsx
@@ -39,6 +39,9 @@ export default function DuvidasEDificuldadesPage() {
                   <Button size="lg" variant="outline" className="text-left justify-start whitespace-normal h-auto">
                     <span className="mr-2">🧠</span> Tenho pensamentos acelerados na hora de dormir
                   </Button>
+                  <Button size="lg" variant="outline" className="text-left justify-start whitespace-normal h-auto">
+                    <span className="mr-2">⏰</span> Acordo sempre no mesmo horário, mesmo sem querer
+                  </Button>
                 </div>
               </CardContent>
             </Card>
